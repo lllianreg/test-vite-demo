77947f3936a6ded68f20fc5991a435218c4770af
CodeSandbox <support@codesandbox.io>
2023-07-07 20:50:06 +0000

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+import { Tests } from "easy-coms-vite";
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <a-button>外部的按钮</a-button>
+    <Tests />
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
